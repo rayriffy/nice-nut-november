6b78589d5133de6ccb9cb30d1f872a37c136a040
Phumrapee Limpianchop <git@rayriffy.com>
2020-11-03 01:07:08 +0900

    fix: script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,20 +12,28 @@ class NextDocument extends Document {
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-2G2JHXEZW8"
           ></script>
-          <script>
-            {`window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-2G2JHXEZW8');`}
-          </script>
-          <script>
-            {`(function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "3xn54cwmzo");`}
-          </script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+    
+                gtag('config', 'G-2G2JHXEZW8');
+              `,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "3xn54cwmzo");
+              `,
+            }}
+          />
         </body>
       </Html>
     )
