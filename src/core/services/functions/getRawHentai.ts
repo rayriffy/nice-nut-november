72acc63cad31e570799b6c1fc1772da1878c73e0
Phumrapee Limpianchop <git@rayriffy.com>
2020-11-13 06:56:50 +0900

    chore: change data endpoint

--- src/core/services/functions/getRawHentai.ts
+++ src/core/services/functions/getRawHentai.ts
@@ -3,4 +3,4 @@ import { restHandler } from './fetch'
 import { RawHentai } from '../../@types/RawHentai'
 
 export const getRawHentai = async (id: number) =>
-  await restHandler<RawHentai>(`https://nhentai.net/api/gallery/${id}`)
+  await restHandler<RawHentai>(`https://h.api.rayriffy.com/v1/gallery/${id}`)
